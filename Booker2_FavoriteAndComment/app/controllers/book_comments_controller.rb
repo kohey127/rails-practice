ece4fd78301e61b756e55f61e70aeeb9a1cf6379
kohey127 <koooochoki@gmail.com>
2020-08-20 09:32:40 +0900

    [FavoriteAndComment]Fix

--- Booker2_FavoriteAndComment/app/controllers/book_comments_controller.rb
+++ Booker2_FavoriteAndComment/app/controllers/book_comments_controller.rb
@@ -1,15 +1,19 @@
 class BookCommentsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(comment_params)
     comment.book_id = book.id
-    comment.save
-    redirect_back(fallback_location: root_path)
+    if comment.save
+      flash[:success] = "Comment was successfully created."
     end
+    redirect_back(fallback_location: root_path)
+  end
 
   def destroy
     book = Book.find(params[:book_id])
-    comment = current_user.book_comments.find_by(book_id: book.id)
+    comment = current_user.book_comments.find_by(book_id: book.id, id: params[:id])
     comment.destroy
     redirect_back(fallback_location: root_path)
   end
