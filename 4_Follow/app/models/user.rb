634db04ca528893a3e8d64883886ed5679cbc0f3
kohey127 <koooochoki@gmail.com>
2020-08-21 10:38:40 +0900

    [4-follow]Finish

--- 4_Follow/app/models/user.rb
+++ 4_Follow/app/models/user.rb
@@ -9,7 +9,29 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   attachment :profile_image, destroy: false
 
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy #フォロー(フォローする、している人)取得、Relationshipのfollower_idにuser_idを格納
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy #フォロワー(フォローされる、されている人)取得、Relationshipのfollowed_idにuser_idを格納
+  has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
+  has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
+  
+
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
   validates :name, length: {maximum: 20, minimum: 2}
   validates :introduction, length: {maximum: 50}
+
+  # ユーザをフォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  # ユーザのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  # フォローしていればtrueを返す
+  def following?(user)
+    following_user.include?(user)
+  end
+
 end
--- 4_Follow/config/routes.rb
+++ 4_Follow/config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users, only: [:show,:index,:edit,:update]
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+
+  post 'follow/:id' => 'relationships#create', as: 'follow' # フォローする
+  delete 'unfollow/:id' => 'relationships#destroy', as: 'unfollow' # フォロー外す
+  get ':user_id/follows' => 'relationships#follows', as: 'follows' # フォロー一覧取得
+  get ':user_id/followers' => 'relationships#followers', as: 'followers' # フォロワー一覧取得
+  
   root 'home#top'
   get 'home/about'
 end
