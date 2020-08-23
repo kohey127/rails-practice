a3b98b9a4f5a8cad08255d2213774c4aa75b3480
kohey127 <koooochoki@gmail.com>
2020-08-23 11:40:36 +0900

    [6-Ajax]Start

---
+++ 6_Ajax/app/models/user.rb
@@ -0,0 +1,50 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+  attachment :profile_image, destroy: false
+
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy #フォロー(フォローする、している人)取得、Relationshipのfollower_idにuser_idを格納
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy #フォロワー(フォローされる、されている人)取得、Relationshipのfollowed_idにuser_idを格納
+  has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
+  has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
+  
+
+  #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
+  validates :name, length: {maximum: 20, minimum: 2}
+  validates :introduction, length: {maximum: 50}
+
+  # ユーザをフォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  # ユーザのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  # フォローしていればtrueを返す
+  def following?(user)
+    following_user.include?(user)
+  end
+
+  def User.search(search, word)
+		if search == "forward_match"
+			User.where("name LIKE?", "#{word}%")
+		elsif search == "backward_match"
+			User.where("name LIKE?", "%#{word}")
+		elsif search == "perfect_match"
+			User.where("name LIKE?", "#{word}")
+		elsif search == "partial_match"
+      User.where("name LIKE?", "%#{word}%")
+    else
+      User.all
+    end
+	end
+end
