894c29d22fdca652729c4dd5906018e62419ec43
kohey127 <koooochoki@gmail.com>
2020-08-24 17:11:27 +0900

    [6-Ajax]Finish

--- 6_Ajax/app/controllers/book_comments_controller.rb
+++ 6_Ajax/app/controllers/book_comments_controller.rb
@@ -2,20 +2,22 @@ class BookCommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(comment_params)
-    comment.book_id = book.id
-    if comment.save
-      flash[:success] = "Comment was successfully created."
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(comment_params)
+    @comment.book_id = @book.id
+    if @comment.save
+      flash.now[:success] = "Comment was successfully created."
     end
-    redirect_back(fallback_location: root_path)
+    render :index
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.find_by(book_id: book.id, id: params[:id])
-    comment.destroy
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.find_by(book_id: @book.id, id: params[:id])
+    if @comment.destroy
+      flash.now[:notice] = "Comment was successfully deleted"
+    end
+    render :index
   end
 
   private
